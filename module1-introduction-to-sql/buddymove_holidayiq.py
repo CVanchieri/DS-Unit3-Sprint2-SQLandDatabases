003623c2341315d042d87cc12b9442fe252e3601
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2019-11-19 18:08:10 -0800

    Update buddymove_holidayiq.py

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -1,8 +1,8 @@
-# Imports
+# imports.
 import pandas as pd
 import sqlite3
 
-# Load the data in df and convert to sqlite3
+# load the data in df and convert to sqlite3.
 df = pd.read_csv('https://raw.githubusercontent.com/CVanchieri/DS-Unit-3-Sprint-2-SQL-and-Databases/master/module1-introduction-to-sql/buddymove_holidayiq.csv')
 # remove the space in 'User ID'.
 df = df.rename(columns={'User Id': 'User_Id'})
@@ -19,7 +19,7 @@ df.to_sql('BuddyMoveHoliday', con=conn)
 #1.count how many rows you have - it should be 249! #249
 q1 = 'SELECT COUNT() \
           FROM  BuddyMoveHoliday;'
-#2.How many users who reviewed at least 100 nature in the category also reviewed at least 100 in the shopping category?
+#2.how many users who reviewed at least 100 nature in the category also reviewed at least 100 in the shopping category?
 q2 = 'SELECT COUNT(User_Id) \
           FROM BuddyMoveHoliday \
           WHERE Nature > 99 AND Shopping > 99;'
@@ -28,18 +28,16 @@ q3 = 'SELECT AVG(Sports), AVG(Religious), AVG(Nature), \
                  AVG(Theatre), AVG(Shopping), AVG(Picnic) \
           FROM BuddyMoveHoliday'
 
-# Execute queries
+# executes.
+#1.count how many rows you have - it should be 249! #249
 curs1 = conn.cursor()
-print('Total # of rows =',
-      curs1.execute(q1).fetchall())
-
+print('Total # of rows =', curs1.execute(q1).fetchall())
+#2.how many users who reviewed at least 100 nature in the category also reviewed at least 100 in the shopping category?
 curs2 = conn.cursor()
-print("Total # of reviews from 'BuddyMoveHoliday' (100+ Nature and 100+ Shopping) =",
-      curs2.execute(q2).fetchall())
-
+print("Total # of reviews from 'BuddyMoveHoliday' (100+ Nature and 100+ Shopping) =", curs2.execute(q2).fetchall())
+#3.(stretch) what are the average number of reviews for each category?
 curs3 = conn.cursor()
-print('Average # of reviews for each category =',
-      curs3.execute(q3).fetchall())
+print('Average # of reviews for each category =', curs3.execute(q3).fetchall())
 
 # close cursors.
 curs1.close()
